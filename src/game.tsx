a5ad439a99bdceff7dda83fbbac64d70402da076
unknown <lstienstra@sogyo.nl>
2026-08-14 16:57:38 +0200

    update

---
+++ src/game.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './game.css'
+
+function Game() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <header>
+      <h1>Welcome to Mancala</h1>
+
+    </header>
+  )
+}
+
+export default Game
